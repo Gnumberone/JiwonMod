e292a0ef6f94616ce3d83a761589e60a504e754e
Gnumberone <35020798+Gnumberone@users.noreply.github.com>
2018-01-01 21:51:44 -0800

    Add files via upload

---
+++ main/java/jiwon/mod/Main.java
@@ -0,0 +1,28 @@
+package jiwon.mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import proxy.CommonProxy;
+import util.Reference;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+	
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {}
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {}
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {}
+}
---
+++ main/java/util/Reference.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Reference 
+{
+	public static final String MODID = "am";
+	public static final String NAME = "Amusement Park Mod";
+	public static final String VERSION = "1.0";
+	public static final String CLIENT = "jiwon.mod.proxy.ClientProxy";
+	public static final String COMMON = "jiwon.mod.proxy.CommonProxy";
+}
